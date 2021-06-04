41c96dd0d4bc7762fad3cbeb46c3df4ee1444575
ArmandoReyesRepo <85260874+ArmandoReyesRepo@users.noreply.github.com>
2021-06-04 02:01:34 -0700

    Add files via upload

---
+++ 11 Cluster Analysis in R/04 Case Study/01 Initial_Expl.R
@@ -0,0 +1,8 @@
+library(dplyr)
+
+
+oes<- readRDS("oes.rds" )
+
+head(oes)
+
+summary(oes)
---
+++ 11 Cluster Analysis in R/04 Case Study/04 Hier_Clust_3.R
@@ -0,0 +1,6 @@
+# View the clustering assignments by sorting the cluster assignment vector
+sort(cut_oes)
+
+# Plot the relationship between mean_salary and year and color the lines by the assigned cluster
+ggplot(gathered_oes, aes(x = year, y = mean_salary, color = factor(cluster))) + 
+  geom_line(aes(group = occupation))
